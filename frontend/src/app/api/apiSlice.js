145d5b22a6596af26e7c1785d5ac1a45732a24c0
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2026-03-18 15:34:46 +0100

    started implementing redux toolkit

---
+++ frontend/src/app/api/apiSlice.js
@@ -0,0 +1,11 @@
+
+
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+
+
+export const apiSlice = createApi({
+    baseQuery:fetchBaseQuery({baseUrl:'http://localhost:5001'}),
+    tagTypes:['Note','User'],
+    endpoints:builder=>({})
+})
